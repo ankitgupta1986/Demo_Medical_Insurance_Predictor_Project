08fc24e4ab0b6a110b0e7d05de3f7f6047cce5d8
Ankit Gupta <127579296+ankitgupta1986@users.noreply.github.com>
2026-03-27 00:00:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import numpy as np
+import pickle
+import joblib
+
+# Load model and scaler
+
+model = joblib.load('insurance_model.pkl')
+scaler = joblib.load('scaler.pkl')
+
+st.title("💊 Medical Insurance Cost Predictor")
+
+st.write("Enter patient details to predict insurance charges")
+
+# User Inputs
+
+age = st.slider("Age", 18, 100, 30)
+
+sex = st.selectbox("Sex", ["Male", "Female"])
+sex = 1 if sex == "Male" else 0
+
+bmi = st.slider("BMI", 10.0, 50.0, 25.0)
+
+children = st.slider("Number of Children", 0, 5, 0)
+
+smoker = st.selectbox("Smoker", ["Yes", "No"])
+smoker = 1 if smoker == "Yes" else 0
+
+region = st.selectbox("Region", ["Northeast", "Northwest", "Southeast", "Southwest"])
+region_dict = {"Northeast":0, "Northwest":1, "Southeast":2, "Southwest":3}
+region = region_dict[region]
+
+# Prediction
+
+if st.button("Predict Insurance Cost"):
+	input_data = np.array([[age, sex, bmi, children, smoker, region]])
+	input_data = scaler.transform(input_data)
+
+	prediction = model.predict(input_data)
+
+	st.success(f"Estimated Insurance Cost: ${prediction[0]:.2f}")
+
